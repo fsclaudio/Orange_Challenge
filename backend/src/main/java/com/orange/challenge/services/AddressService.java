fc82037394ec41875504bde72f89eb11cb106e1f
fsclaudio <fsclaudio@gmail.com>
2021-04-18 13:19:19 -0300

    Find Address by id and exception handling

--- backend/src/main/java/com/orange/challenge/services/AddressService.java
+++ backend/src/main/java/com/orange/challenge/services/AddressService.java
@@ -1,6 +1,7 @@
 package com.orange.challenge.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.orange.challenge.dto.AddressDTO;
 import com.orange.challenge.entyties.Address;
 import com.orange.challenge.repositories.AddressRepository;
+import com.orange.challenge.services.exceptions.EntityNotFoundException;
 
 @Service
 public class AddressService {
@@ -20,8 +22,14 @@ public class AddressService {
 	@Transactional(readOnly = true)
 	public List<AddressDTO> findAll(){
 		List<Address> list= repository.findAll();
-		return list.stream().map(x -> new AddressDTO(x)).collect(Collectors.toList());
-		
+		return list.stream().map(x -> new AddressDTO(x)).collect(Collectors.toList());	
+	}
+	
+	@Transactional(readOnly = true) 
+	public AddressDTO findById(Long id) {
+		Optional<Address> obj = repository.findById(id);
+		Address entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new AddressDTO(entity);
 	}
 
 }
---
+++ backend/src/main/java/com/orange/challenge/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.orange.challenge.services.exceptions;
+
+public class EntityNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public EntityNotFoundException(String msg) {
+		super(msg);
+	}
+}
